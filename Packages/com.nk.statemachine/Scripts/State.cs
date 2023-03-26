15a2de8f590ab7153c5a3f4d42229c1cf172b5f7
Naohiro Kimura <155drops@gmail.com>
2023-03-26 15:32:04 +0900

    無視リストの更新とステートマシンのスクリプト追加

--- Packages/com.nk.statemachine/Scripts/State.cs
+++ Packages/com.nk.statemachine/Scripts/State.cs
@@ -1,12 +1,33 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CP.StateMachineSystem;
 
-namespace CP.Statemachine
+namespace CP.StateMachineSystem
 {
-    public class State<T>
-    {
-        
-    }
-}
+	[System.Serializable]
+	public abstract class State<T>
+	{
+		protected T Owner;
+		private GameStateBase owner;
 
+		public State(T owner)
+		{
+			Owner = owner;
+		}
+
+		protected State(GameStateBase owner)
+		{
+			this.owner = owner;
+		}
+
+		public virtual void StateUpdate(T owner) { }
+		public virtual void StateEnter(T owner) { }
+		public virtual void StateExit(T owner) { }
+		public virtual IEnumerator StateUpdateEn(T owner) { yield break; }
+		public virtual void StateFixeUpdate(T owner) { }
+	}
+
+	public class GameStateBase{}
+
+}
